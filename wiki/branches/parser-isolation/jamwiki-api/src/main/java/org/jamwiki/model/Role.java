97844d138a7dc9930eecb8365a74b98e41e702f8
decamps <decamps@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2007-08-02 19:21:01 +0000

    * revert r1737 with Role impl * move role to jamwiki-api

--- wiki/branches/parser-isolation/jamwiki-api/src/main/java/org/jamwiki/model/Role.java
+++ wiki/branches/parser-isolation/jamwiki-api/src/main/java/org/jamwiki/model/Role.java
@@ -1,35 +1,62 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, version 2.1, dated February 1999.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the latest version of the GNU Lesser General
+ * Public License as published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program (LICENSE.txt); if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package org.jamwiki.model;
 
-import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.GrantedAuthorityImpl;
 
-public interface Role extends GrantedAuthority {
-	public static final String ROLE_ADMIN = new String("ROLE_ADMIN");
-	/** ROLE_ANONYMOUS is not store in the database but is instead automatically assigned to all non-logged in users. */
-	public static final String ROLE_ANONYMOUS = new String("ROLE_ANONYMOUS");
-	public static final String ROLE_DELETE = new String("ROLE_DELETE");
-	public static final String ROLE_EDIT_EXISTING = new String(
-			"ROLE_EDIT_EXISTING");
-	public static final String ROLE_EDIT_NEW = new String("ROLE_EDIT_NEW");
-	public static final String ROLE_MOVE = new String("ROLE_MOVE");
-	public static final String ROLE_TRANSLATE = new String("ROLE_TRANSLATE");
-	public static final String ROLE_UPLOAD = new String("ROLE_UPLOAD");
-	/** ROLE_USER is not store in the database but is instead automatically assigned to all logged in users. */
-	public static final String ROLE_USER = new String("ROLE_USER");
-	public static final String ROLE_VIEW = new String("ROLE_VIEW");
+/**
+ * Provides an object representing a Wiki role and implementing the Acegi
+ * <code>GrantedAuthority</code> interface.
+ */
+public class Role  extends GrantedAuthorityImpl  {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7995231871249892674L;
+	private String description = null;
 
 	/**
 	 *
 	 */
-	public abstract String getDescription();
+	public Role(String role) {
+		super((role == null) ? null : role.toUpperCase());
+	}
 
-	/**
-	 *
+	/* (non-Javadoc)
+	 * @see org.jamwiki.model.Role#getDescription()
 	 */
-	public abstract void setDescription(String description);
+	public String getDescription() {
+		return this.description;
+	}
 
-	/**
-	 * Two roles are equal if the role names are the same.
+	/* (non-Javadoc)
+	 * @see org.jamwiki.model.Role#setDescription(java.lang.String)
 	 */
-	public abstract boolean equals(Role role);
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
+	/* (non-Javadoc)
+	 * @see org.jamwiki.model.Role#equals(org.jamwiki.model.WikiRole)
+	 */
+	public boolean equals(Role role) {
+		if (this.getAuthority() == null && role != null && role.getAuthority() == null) {
+			return true;
+		}
+		return (this.getAuthority() != null && role != null && this.getAuthority().equals(role.getAuthority()));
+	}
 }
