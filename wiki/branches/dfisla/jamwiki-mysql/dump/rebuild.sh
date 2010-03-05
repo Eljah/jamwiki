7d3d2feafc2bcd7a5c587a0954f747839e2182d7
dfisla <dfisla@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2010-03-05 18:35:16 +0000

    purging of files, cleanup

--- wiki/branches/dfisla/jamwiki-mysql/dump/rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mysql -u admin --password=slivovica -e 'drop database jamwiki;'
-mysql -u admin --password=slivovica -e 'create database jamwiki character set utf8;'
-#mysql -u admin --password=slivovica jamwiki < jamwiki-clean-install-mysqldump-new2-noFK.sql
-#mysql -u admin --password=slivovica jamwiki < jamwiki-clean-install-mysqldump-new3-utf-bin-blob.sql
-#mysql -u admin --password=slivovica jamwiki < jamwiki-clean-install-mysqldump-new4-utf-bin-blob-MyISAM.sql
-#mysql -u admin --password=slivovica jamwiki < jamwiki-clean-install-mysqldump-new6-utf-bin-blob-InnoDB.sql
-mysql -u admin --password=slivovica jamwiki < jamwiki-clean-install-mysqldump-new6-utf-bin-blob-MyISAM.sql
