aa0ad2ca9a5e7fedd39b78f881918bc54c1b60be
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2011-09-23 02:16:49 +0000

    Parser:   * Add some validation logic to WikiLink.setQuery and WikiLink.setSection.   * Add additional Javadoc to the WikiLink methods.

--- wiki/trunk/jamwiki-core/src/main/java/org/jamwiki/parser/WikiLink.java
+++ wiki/trunk/jamwiki-core/src/main/java/org/jamwiki/parser/WikiLink.java
@@ -16,6 +16,7 @@
  */
 package org.jamwiki.parser;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jamwiki.model.Interwiki;
 import org.jamwiki.model.Namespace;
 import org.jamwiki.model.VirtualWiki;
@@ -67,14 +68,18 @@ public class WikiLink {
 	}
 
 	/**
-	 *
+	 * Return the topic that this link points to, not including the namespace.
+	 * May be <code>null</code> if (for example) this link is to a section within
+	 * the current article of the form "#Section".
 	 */
 	public String getArticle() {
 		return this.article;
 	}
 
 	/**
-	 *
+	 * Set the topic that this link points to, not including the namespace.
+	 * May be <code>null</code> if (for example) this link is to a section within
+	 * the current article of the form "#Section".
 	 */
 	public void setArticle(String article) {
 		this.article = article;
@@ -95,42 +100,49 @@ public class WikiLink {
 	}
 
 	/**
-	 *
+	 * Return the topic that this link points to, including the namespace.  May
+	 * be <code>null</code> if (for example) this link is to a section within
+	 * the current article of the form "#Section".
 	 */
 	public String getDestination() {
 		return this.destination;
 	}
 
 	/**
-	 *
+	 * Set the topic that this link points to, including the namespace.  May
+	 * be <code>null</code> if (for example) this link is to a section within
+	 * the current article of the form "#Section".
 	 */
 	public void setDestination(String destination) {
 		this.destination = destination;
 	}
 
 	/**
-	 *
+	 * Return the interwiki link object if this wiki link references is to
+	 * a configured interwiki.
 	 */
 	public Interwiki getInterwiki() {
 		return this.interwiki;
 	}
 
 	/**
-	 *
+	 * Set the interwiki link object if this wiki link references is to
+	 * a configured interwiki.
 	 */
 	public void setInterwiki(Interwiki interwiki) {
 		this.interwiki = interwiki;
 	}
 
 	/**
-	 *
+	 * Return the namespace object for this wiki link's target topic, or
+	 * <code>null</code> if there is no target topic.
 	 */
 	public Namespace getNamespace() {
 		return this.namespace;
 	}
 
 	/**
-	 *
+	 * Set the namespace object for this wiki link's target topic.
 	 */
 	public void setNamespace(Namespace namespace) {
 		if (namespace == null) {
@@ -140,31 +152,49 @@ public class WikiLink {
 	}
 
 	/**
-	 *
+	 * Return the query string that this link refers to ("?param=value").
+	 * This method returns the query string without the opening "?", or
+	 * <code>null</code> if there is no query string for this link.
 	 */
 	public String getQuery() {
 		return this.query;
 	}
 
 	/**
-	 *
+	 * Set the query string that this link refers to ("?param=value").
+	 * Minimal processing is done by this method to strip any opening "?"
+	 * and to ensure that the supplied value has non-whitespace content.
 	 */
 	public void setQuery(String query) {
-		this.query = query;
+		if (!StringUtils.isBlank(query)) {
+			if (query.trim().startsWith("?")) {
+				query = query.trim().substring(1);
+			}
+			this.query = query;
+		}
 	}
 
 	/**
-	 *
+	 * Return the section within a page that this link refers to ("#Section").
+	 * This method returns the section name without the opening "#", or
+	 * <code>null</code> if there is no section for this link.
 	 */
 	public String getSection() {
 		return this.section;
 	}
 
 	/**
-	 *
+	 * Set the section within a page that this link refers to ("#Section").
+	 * Minimal processing is done by this method to strip any opening "#"
+	 * and to ensure that the supplied value has non-whitespace content.
 	 */
 	public void setSection(String section) {
-		this.section = section;
+		if (!StringUtils.isBlank(section)) {
+			if (section.trim().startsWith("#")) {
+				section = section.trim().substring(1);
+			}
+			this.section = section.trim();
+		}
 	}
 
 	/**
