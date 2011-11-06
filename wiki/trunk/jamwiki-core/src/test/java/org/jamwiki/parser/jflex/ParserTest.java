53421c349b4e73f89dda3490bbb0f29ffeafb6f2
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2011-11-06 19:39:20 +0000

    Parser:   * Enable a parser test that is no longer failing.

--- wiki/trunk/jamwiki-core/src/test/java/org/jamwiki/parser/jflex/ParserTest.java
+++ wiki/trunk/jamwiki-core/src/test/java/org/jamwiki/parser/jflex/ParserTest.java
@@ -323,7 +323,6 @@ public class ParserTest extends JAMWikiUnitTest {
 		failures.add("NestedTable1"); // paragraphs in <td> tags
 		failures.add("Paragraph12"); // no paragraph within div without newline
 		failures.add("Paragraph13"); // paragraphs in <td> tags
-		failures.add("Paragraph15"); // failure parsing of empty paragraphs with <nowiki>
 		failures.add("ParserFunctionsBroken");
 		failures.add("PreformattedInvalid1");
 		failures.add("Template_-_Paramtest"); // issues with params when parsing a template page
