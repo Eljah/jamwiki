3aa9f55852a5bd8085204079bc5e905cb006cb0a
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2011-11-27 18:26:14 +0000

    Merge:   * Merge changes from the 1.1.x branch to trunk.

--- wiki/trunk/jamwiki-core/src/main/java/org/jamwiki/db/AnsiDataHandler.java
+++ wiki/trunk/jamwiki-core/src/main/java/org/jamwiki/db/AnsiDataHandler.java
@@ -688,11 +688,14 @@ public class AnsiDataHandler implements DataHandler {
 			return (Map<Object, UserBlock>)cacheElement.getObjectValue();
 		}
 		Map<Object, UserBlock> userBlocks = new LinkedHashMap<Object, UserBlock>();
+		Connection conn = null;
 		try {
-			Connection conn = DatabaseConnection.getConnection();
+			conn = DatabaseConnection.getConnection();
 			userBlocks = this.queryHandler().getUserBlocks(conn);
 		} catch (SQLException e) {
 			throw new DataAccessException(e);
+		} finally {
+			DatabaseConnection.closeConnection(conn);
 		}
 		WikiCache.addToCache(CACHE_USER_BLOCKS_ACTIVE, CACHE_USER_BLOCKS_ACTIVE, userBlocks);
 		return userBlocks;
@@ -722,11 +725,14 @@ public class AnsiDataHandler implements DataHandler {
 			return (List<VirtualWiki>)cacheElement.getObjectValue();
 		}
 		List<VirtualWiki> virtualWikis = new ArrayList<VirtualWiki>();
+		Connection conn = null;
 		try {
-			Connection conn = DatabaseConnection.getConnection();
+			conn = DatabaseConnection.getConnection();
 			virtualWikis = this.queryHandler().getVirtualWikis(conn);
 		} catch (SQLException e) {
 			throw new DataAccessException(e);
+		} finally {
+			DatabaseConnection.closeConnection(conn);
 		}
 		WikiCache.addToCache(CACHE_VIRTUAL_WIKI_LIST, CACHE_VIRTUAL_WIKI_LIST, virtualWikis);
 		return virtualWikis;
@@ -1248,15 +1254,19 @@ public class AnsiDataHandler implements DataHandler {
 			return (WikiUser)cacheElement.getObjectValue();
 		}
 		WikiUser result = null;
+		TransactionStatus status = null;
 		try {
+			status = DatabaseConnection.startTransaction();
 			Connection conn = DatabaseConnection.getConnection();
 			int userId = this.queryHandler().lookupWikiUser(username, conn);
 			if (userId != -1) {
 				result = lookupWikiUser(userId);
 			}
 		} catch (SQLException e) {
+			DatabaseConnection.rollbackOnException(status, e);
 			throw new DataAccessException(e);
 		}
+		DatabaseConnection.commit(status);
 		WikiCache.addToCache(CACHE_USER_BY_USER_NAME, username, result);
 		return result;
 	}
