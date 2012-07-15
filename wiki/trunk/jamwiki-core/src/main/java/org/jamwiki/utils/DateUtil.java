692336332a2669e3f91a88ff605a77aa2a2effd5
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2012-07-15 23:45:54 +0000

    Documentation:   * Add license header and class description to new DateUtil class.

--- wiki/trunk/jamwiki-core/src/main/java/org/jamwiki/utils/DateUtil.java
+++ wiki/trunk/jamwiki-core/src/main/java/org/jamwiki/utils/DateUtil.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, version 2.1, dated February 1999.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the latest version of the GNU Lesser General
+ * Public License as published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program (LICENSE.txt); if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package org.jamwiki.utils;
 
 import java.text.DateFormat;
@@ -10,8 +26,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.jamwiki.Environment;
 import org.jamwiki.model.WikiUser;
 
-
-
+/**
+ * Utility methods for working with dates and time zones.
+ */
 public class DateUtil {
 
 	/**
