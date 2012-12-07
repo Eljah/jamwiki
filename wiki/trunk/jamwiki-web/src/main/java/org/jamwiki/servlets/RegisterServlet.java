8889d5f77823f01e86d1435fd0620cb4dc105194
wrh2 <wrh2@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2012-12-07 00:39:00 +0000

    Special:Account:   * No need to return an error message due to an invalid date format since     the user can't do anything about it.  Just log a message for the system     administrator.

--- wiki/trunk/jamwiki-web/src/main/java/org/jamwiki/servlets/RegisterServlet.java
+++ wiki/trunk/jamwiki-web/src/main/java/org/jamwiki/servlets/RegisterServlet.java
@@ -16,6 +16,7 @@
  */
 package org.jamwiki.servlets;
 
+import java.text.SimpleDateFormat;
 import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -35,7 +36,6 @@ import org.jamwiki.parser.ParserException;
 import org.jamwiki.parser.ParserInput;
 import org.jamwiki.parser.ParserOutput;
 import org.jamwiki.parser.jflex.JFlexParser;
-import org.jamwiki.utils.DateUtil;
 import org.jamwiki.utils.Encryption;
 import org.jamwiki.utils.WikiLogger;
 import org.jamwiki.utils.WikiUtil;
@@ -245,11 +245,12 @@ public class RegisterServlet extends JAMWikiServlet {
 		if (result != null) {
 			pageInfo.addError(new WikiMessage("edit.exception.spam", result));
 		}
-		String dateFormat = request.getParameter("datetimeFormat");
 		try {
-			DateUtil.getUserLocalTime(null, dateFormat, null);
+			new SimpleDateFormat(request.getParameter(WikiUser.USER_PREFERENCE_DATETIME_FORMAT));
 		} catch (IllegalArgumentException e) {
-			pageInfo.addError(new WikiMessage("register.error.dateinvalid", dateFormat));
+			// the format will never be null in a properly configured wiki, so this
+			// test is mostly for administrators
+			logger.error("Invalid date format configured, please check wiki configuration", e);
 		}
 	}
 
