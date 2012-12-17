043955bc7c5685b4d9f64a15ae3593ded2e27ed4
cclavadetscher <cclavadetscher@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2012-12-17 10:31:06 +0000

    Addition of two new properties for configuring the password reset feature: - smtp-service-forgot-password-challenge-timeout: The validity duration of a challenge that has been sent to a user. - smtp-service-forgot-password-challenge-retries: The maximum number of requests allowed in the timeout period from the same IP.
    TBD: Changes to admin page to enable configuration of these values.

--- wiki/branches/cclavadetscher/jamwiki-core/src/main/java/org/jamwiki/Environment.java
+++ wiki/branches/cclavadetscher/jamwiki-core/src/main/java/org/jamwiki/Environment.java
@@ -78,6 +78,8 @@ public class Environment {
 	public static final String PROP_EMAIL_ADDRESS_SEPARATOR = "smtp-address-separator";
 	public static final String PROP_EMAIL_DEFAULT_CONTENT_TYPE = "smtp-content-type";
 	public static final String PROP_EMAIL_SERVICE_FORGOT_PASSWORD = "smtp-service-forgot-password";
+	public static final String PROP_EMAIL_SERVICE_FORGOT_PASSWORD_CHALLENGE_TIMEOUT = "smtp-service-forgot-password-challenge-timeout";
+	public static final String PROP_EMAIL_SERVICE_FORGOT_PASSWORD_CHALLENGE_RETRIES = "smtp-service-forgot-password-challenge-retries";
 	public static final String PROP_ENCRYPTION_ALGORITHM = "encryption-algorithm";
 	public static final String PROP_EXTERNAL_LINK_NEW_WINDOW = "external-link-new-window";
 	public static final String PROP_FILE_BLACKLIST = "file-blacklist";
@@ -236,6 +238,8 @@ public class Environment {
 		this.defaults.setProperty(PROP_EMAIL_ADDRESS_SEPARATOR,";");
 		this.defaults.setProperty(PROP_EMAIL_DEFAULT_CONTENT_TYPE,"text/plain");
 		this.defaults.setProperty(PROP_EMAIL_SERVICE_FORGOT_PASSWORD, Boolean.FALSE.toString());
+		this.defaults.setProperty(PROP_EMAIL_SERVICE_FORGOT_PASSWORD_CHALLENGE_TIMEOUT, "60");
+		this.defaults.setProperty(PROP_EMAIL_SERVICE_FORGOT_PASSWORD_CHALLENGE_RETRIES, "3");
 		this.defaults.setProperty(PROP_ENCRYPTION_ALGORITHM, "SHA-512");
 		this.defaults.setProperty(PROP_EXTERNAL_LINK_NEW_WINDOW, Boolean.FALSE.toString());
 		this.defaults.setProperty(PROP_FILE_BLACKLIST, "bat,bin,exe,htm,html,js,jsp,php,sh");
