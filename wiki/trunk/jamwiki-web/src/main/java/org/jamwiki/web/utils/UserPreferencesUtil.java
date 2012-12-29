a623af80b760ecaacd68cac2e15b77d30a7d8e32
cclavadetscher <cclavadetscher@f65b7e76-0091-dd46-87ae-4bc0c949c03e>
2012-12-29 10:10:17 +0000

    Modified retrieval of list of locales for user language preferences from system to available translations as listed in jamwiki-configuration-x.x.xml.

--- wiki/trunk/jamwiki-web/src/main/java/org/jamwiki/web/utils/UserPreferencesUtil.java
+++ wiki/trunk/jamwiki-web/src/main/java/org/jamwiki/web/utils/UserPreferencesUtil.java
@@ -151,6 +151,7 @@ public class UserPreferencesUtil {
 			if (prefName.equals(WikiUser.USER_PREFERENCE_TIMEZONE)) {
 				return DateUtil.getTimeZoneMap();
 			} else if (prefName.equals(WikiUser.USER_PREFERENCE_DEFAULT_LOCALE)) {
+				/*
 				LinkedHashMap<String, String> locales = new LinkedHashMap<String, String>();
 				Locale[] localeArray = Locale.getAvailableLocales();
 				for (int i = 0; i < localeArray.length; i++) {
@@ -159,6 +160,8 @@ public class UserPreferencesUtil {
 					locales.put(key, value);
 				}
 				return locales;
+				*/
+				return WikiConfiguration.getInstance().getTranslations();
 			} else if (prefName.equals(WikiUser.USER_PREFERENCE_PREFERRED_EDITOR)) {
 				return WikiConfiguration.getInstance().getEditors();
 			} else if (prefName.equals(WikiUser.USER_PREFERENCE_DATE_FORMAT)) {
